2db372fe306904a14fdb95c214d504bdc80d46d3
Albertus A <angkuwjr@yahoo.com>
2023-10-09 11:18:46 +0700

    Memperbaiki Dockerfile dan build push shell

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -3,8 +3,8 @@
 
 docker login ghcr.io -u="${GITHUB_USERNAME}" -p="${GITHUB_TOKEN}"
 
-docker build -t albertushub/karsajobs:latest
+docker build -t albertushub/karsajobs:latest .
 
-docker push albertushub/karsajobs:latest
+docker push ghcr.io/albertushub/karsajobs:latest
 
 docker logout
