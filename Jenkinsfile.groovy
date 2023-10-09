84eba2273e11d3704e8b6b3e32531084b1cd64a7
Albertus A <angkuwjr@yahoo.com>
2023-10-09 11:10:22 +0700

    Progress mengerjakan submission backend

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,26 +3,37 @@
 pipeline{
     agent any
     stages{
-        stage("lint-dockerfile"){
-            agent {
-                docker {
-                    image 'hadolint/hadolint:latest-alpine'
-                }
-            }
-            steps{
-                sh 'hadolint -v'
-                sh 'hadolint Dockerfile'
-            }
-        }
+        // stage("lint-dockerfile"){
+        //     agent {
+        //         docker {
+        //             image 'hadolint/hadolint:latest-alpine'
+        //         }
+        //     }
+        //     steps{
+        //         sh 'hadolint -v'
+        //         sh 'hadolint Dockerfile -V'
+        //     }
+        // }
         stage("test-app"){
             agent{
                 docker {
-                    image 'golang:alpine'
+                    image 'golang:1.15-alpine'
                 }
             }
+            environment {
+        		GOBIN = '/tmp/go-bin'
+        		GOCACHE = '/tmp/go-build'
+        		CGO_ENABLED=0
+        		HOME = '/tmp'
+        	}
             steps{
-                sh 'go version'
-                sh 'go test -v -short --count=1 $(go list ./...)'
+                script{
+                    sh 'go version'
+                    sh 'env'
+                    sh 'ls'
+                    sh 'go mod download'
+                    sh 'go test -v -short --count=1 $(go list ./...)'
+                }
             }
         }
         stage("build-app-karsajobs"){
@@ -30,7 +41,7 @@ pipeline{
                 withCredentials([usernamePassword(credentialsId: 'd4ef32a9-8bb4-4e57-a8d7-0a080dd7c5d4', 
                                                     passwordVariable: 'GITHUB_TOKEN',   
                                                     usernameVariable: 'GITHUB_USERNAME')]) {
-                    sh 'build_push_image_karsajobs.sh'
+                    sh './build_push_image_karsajobs.sh'
                 }
             }
         }
